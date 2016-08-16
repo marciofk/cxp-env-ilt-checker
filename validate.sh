542977e889d6c0279d695bd44fdaf2bb4ab69ad5
Marcio Fuckner <marcio@backbase.com>
2016-08-16 19:16:24 +0200

    script enhancements

--- validate.sh
+++ validate.sh
@@ -11,7 +11,20 @@
 # Return: none
 # -----------------------------------------------------------------------------
 function log {
-	echo -e $(date +"%D %T") [$1] - $2
+	color_mod="\033[1;49m"
+
+	if [[ $1 == "ERROR" ]] 
+	then
+		color_mod="\033[1;31m"
+	elif [[ $1 == "TITLE" ]] 
+	then
+		color_mod="\033[1;32m"
+	elif [[ $1 == "WARNING" ]] 
+	then
+		color_mod="\033[1;93m"
+	fi		
+
+	echo -e "$color_mod $(date +'%D %T') [$1] $2\033[0m" 
 }
 
 # -----------------------------------------------------------------------------
@@ -23,28 +36,28 @@ function check_variables {
 	# Java Home variable
 	if [ -z "$JAVA_HOME" ]
 	then
-   		log "ERROR" "Empty JAVA_HOME variable. Please set this environment variable with your JDK home directory"
+   		log "ERROR" "\tEmpty JAVA_HOME variable. Please set this environment variable to your JDK home directory"
    		var_error=true
 	fi
 
 	# M2_HOME variable
 	if [ -z "$M2_HOME" ]
 	then
-	   log "ERROR" "Empty M2_HOME variable. Please set this environment variable with your MAVEN home directory"
+	   log "ERROR" "\tEmpty M2_HOME variable. Please set this environment variable to your MAVEN home directory"
 	   var_error=true
 	fi
 
 	# M2 variable
 	if [ -z "$M2" ]
 	then
-	   log "ERROR" "Empty M2 variable. Please set this environment variable with your MAVEN bin directory"
+	   log "ERROR" "\tEmpty M2 variable. Please set this environment variable to your MAVEN bin directory"
 	   var_error=true
 	fi
 
 	# MAVEN_OPTS variable
 	if [ -z "$MAVEN_OPTS" ]
 	then
-	   log "ERROR" "Empty MAVEN_OPTS variable. Please set this environment variable"
+	   log "ERROR" "\tEmpty MAVEN_OPTS. Please set this environment variable"
 	   var_error=true
 	fi	
 	if $var_error; then
@@ -66,23 +79,23 @@ function check_java_installation {
 
 	if [ -z $java_binary ]
 	then
-  		log "ERROR" "Java runtime is not present in the path. Please, fix your PATH variable"
+  		log "ERROR" "\tJava runtime is not present in the path. Please, fix your PATH variable"
   		return -1
 	fi
 
 	java_folder=$(echo $java_binary | sed 's/\/bin\/java$//g')
 	if [ $JAVA_HOME != $java_folder ]
 	then
-   		log "ERROR" "Java runtime conflict: Your java binary present in the path is pointing to a different java installation. Please check your PATH and JAVA_HOME variables"  
+   		log "ERROR" "\tJava runtime conflict: Your java binary present in the path is pointing to a different java installation. Please check your PATH and JAVA_HOME variables"  
    		return -1
 	fi
 
 	# Checking Java version
-	java_version=$(java -version 2>&1 | head -1 | sed "s/java version//g" | sed 's/"//g' | sed "s/\.//g" | sed "s/_.*//g")
 	java_version_pretty=$(java -version 2>&1 | head -1 | sed "s/java version//g" | sed 's/"//g')
+	java_version=$(echo $java_version_pretty | sed 's/"//g' | sed "s/\.//g" | sed "s/_.*//g")
 
 	if (( java_version < 180 )); then
-		log "ERROR" "Update your JDK installation to at least version 1.8"
+		log "ERROR" "\tUpdate your JDK installation to at least version 1.8"
 		return -1
 	fi
 	log "INFO" "\tJava version: $java_version_pretty"
@@ -99,17 +112,18 @@ function check_git_installation {
 
 	if [ -z $git_binary ]
 	then
-  		echo "Your git binary is not present in the path. Please, check the PATH variable"
+  		echo "\tGit binary is not present in the path. Please, check the PATH variable"
   		return -1
 	fi
 
-	git_version=$(git --version | sed "s/git version//g" | sed "s/\.//g")
-	git_version_pretty=$(git --version | sed "s/git version//g")
+    git_version_pretty=$(git --version | sed "s/git version//g")
+	git_version=$(echo $git_version_pretty | sed "s/\.//g")
+	
 	if (( git_version < 200 )); then
-		log "ERROR" "Update your git to the latest version"
+		log "ERROR" "\tUpdate your git to the latest version"
 		return -1
 	fi
-	log "INFO" "\tgit version: $git_version_pretty"
+	log "INFO" "\tGit version: $git_version_pretty"
 	return 0
 }
 
@@ -122,7 +136,7 @@ function check_mvn_installation {
 
 	if [ -z $mvn_binary ]
 	then
-  		echo "Your maven binary is not present in the path. Please, check the PATH variable"
+  		echo "\tMaven binary is not present in the path"
   		return -1
 	fi
 
@@ -131,10 +145,10 @@ function check_mvn_installation {
 	mvn_version_pretty=$(mvn --version 2>&1 | grep "Apache Maven" | sed "s/(.*)//g")
 
 	if (( mvn_version < 300 )); then
-		log "ERROR" "Update your maven installation to the latest version"
+		log "ERROR" "\tUpdate your maven installation to the latest version"
 		return -1
 	fi
-	log "INFO" "\tmaven version: $mvn_version_pretty"
+	log "INFO" "\tMaven version: $mvn_version_pretty"
 	return 0
 }
 
@@ -142,7 +156,7 @@ function check_mvn_repo {
 	rm -rf repo-test 2> /dev/null
 	git clone https://github.com/marciofk/repo-test.git 2> /dev/null
 	if [ $? -ne 0 ]; then
-    	log "ERROR" "Error cloning test project. Please check your network settings"
+    	log "ERROR" "\tError cloning test project. Please check your network settings"
     	return -1
 	fi
 	cd repo-test
@@ -151,13 +165,22 @@ function check_mvn_repo {
 	before=$(date +%s)
 	mvn -Dmaven.repo.local=localrepo dependency:purge-local-repository clean install >/dev/null 2>&1
 	if [ $? -ne 0 ]; then
-    	log "ERROR" "Error downloading repositories. Check your maven configuration and network settings"
+    	log "ERROR" "\tError downloading repositories. Check your maven configuration and network settings"
     	return -1
 	fi
 	after=$(date +%s)
 	let total_time=after-before
 	cd ..
-	log "INFO" "\tTotal time: $total_time $before $after"
+	log "INFO" "\tTotal time: $total_time seconds"
+
+	if (( total_time > 300 && total_time < 600)); then
+		log "WARNING" "\tThe amount of time to download some artifacts took some time ($total_time sec). Please check your internet connection"
+	fi
+
+	if (( total_time >= 600)); then
+		log "ERROR" "\tThe amount of time to download some artifacts took too long ($total_time sec). Please check your internet connection"
+	fi
+
 	return 0
 }
 
@@ -166,12 +189,12 @@ function check_hardware {
 
 	if (( memory < 8 && memory >= 4))
 	then
-   		log "WARNING" "The available memory of $memory GB is below the minimum amount required (8 GB)"
+   		log "WARNING" "\tThe available memory of $memory GB is below the minimum amount required (8 GB)"
 	fi
 
 	if (( memory < 4))
 	then
-   		log "ERROR" "The available memory of $memory GB is below the minimum amount required (8 GB)"
+   		log "ERROR" "\tThe available memory of $memory GB is below the minimum amount required (8 GB)"
 	fi
 
 	# Checking processors
@@ -180,7 +203,7 @@ function check_hardware {
 
 	if (( cores * processor < 2))
 	then
-   		log "WARNING" "Your processor configuration is below the minimum requirement. It is recommended using a multicore or multiprocessor machine"
+   		log "WARNING" "\tYour processor configuration is below the minimum requirement. We recommend using a multicore/multiprocessor machine"
 	fi
 	log "INFO" "\tMemory: $memory GB"
 	log "INFO" "\t# of cores: $cores"
@@ -192,23 +215,23 @@ function check_hardware {
 # Validation body
 # -----------------------------------------------------------------------------
 
-log "INFO" "Checking environment variables"
+log "TITLE" "Checking environment variables"
 check_variables
 
 if [ $? -ne 0 ]; then
-    log "ERROR" "Aborting validation. Please correct the errors and try again"
+    log "ERROR" "\tAborting validation. Please correct the errors and try again"
     exit -1
 fi
 
-log "INFO" "Checking java installation"
+log "TITLE" "Checking Java installation"
 check_java_installation
 
 if [ $? -ne 0 ]; then
-    log "ERROR" "Aborting validation. Please correct the errors and try again"
+    log "ERROR" "\tAborting validation. Please correct the errors and try again"
     exit -1
 fi
 
-log "INFO" "Checking git installation"
+log "TITLE" "Checking Git installation"
 check_git_installation
 
 if [ $? -ne 0 ]; then
@@ -216,7 +239,7 @@ if [ $? -ne 0 ]; then
     exit -1
 fi
 
-log "INFO" "Checking maven installation"
+log "TITLE" "Checking Maven installation"
 check_mvn_installation
 
 if [ $? -ne 0 ]; then
@@ -224,7 +247,7 @@ if [ $? -ne 0 ]; then
     exit -1
 fi
 
-log "INFO" "Checking maven repository access"
+log "TITLE" "Checking Maven repository access"
 check_mvn_repo
 
 if [ $? -ne 0 ]; then
@@ -232,7 +255,7 @@ if [ $? -ne 0 ]; then
     exit -1
 fi
 
-log "INFO" "Checking hardware"
+log "TITLE" "Checking hardware"
 check_hardware
 
 if [ $? -ne 0 ]; then
@@ -240,5 +263,5 @@ if [ $? -ne 0 ]; then
     exit -1
 fi
 
-log "INFO" "Finished"
+log "TITLE" "Finished"
 
